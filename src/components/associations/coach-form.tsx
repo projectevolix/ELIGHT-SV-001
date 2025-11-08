7899225e676ac1c3ce94ffe89912875d7338d7e1
Oshan Avishka <oshanavishka6@gmail.com>
2025-11-08 05:14:02 +0000

    find date selectors of this hole application it has bad ux because we us

--- src/components/associations/coach-form.tsx
+++ src/components/associations/coach-form.tsx
@@ -164,6 +164,9 @@ export function CoachForm({ mode, coach, onSave, onCancel }: CoachFormProps) {
                   </PopoverTrigger>
                   <PopoverContent className="w-auto p-0" align="start">
                     <Calendar
+                      captionLayout="dropdown-buttons"
+                      fromYear={1900}
+                      toYear={new Date().getFullYear()}
                       mode="single"
                       selected={field.value}
                       onSelect={field.onChange}
--- src/components/associations/player-form.tsx
+++ src/components/associations/player-form.tsx
@@ -147,6 +147,9 @@ export function PlayerForm({ mode, player, onSave, onCancel }: PlayerFormProps) 
                   </PopoverTrigger>
                   <PopoverContent className="w-auto p-0" align="start">
                     <Calendar
+                      captionLayout="dropdown-buttons"
+                      fromYear={1900}
+                      toYear={new Date().getFullYear()}
                       mode="single"
                       selected={field.value}
                       onSelect={field.onChange}
