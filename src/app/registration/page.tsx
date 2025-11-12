b0603503c5b00157f4981b03533e4348e94d231e
Oshan Avishka <oshanavishka6@gmail.com>
2025-11-12 06:41:11 +0000

    change to "public draw"  (_for element <Button>_)

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -140,7 +140,7 @@ export default function RegistrationPage() {
                     </Select>
                 </div>
                 <div className="ml-auto">
-                    <Button>Publish Event</Button>
+                    <Button>public draw</Button>
                 </div>
             </div>
 
